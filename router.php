24b3747833cfc78612462de4358b5f2d35d84f21
Henrico <henricoleodra@gmail.com>
2019-04-29 14:15:02 +0700

    Upload File

--- router.php
+++ router.php
@@ -1,11 +1,13 @@
-<$php
-	<!DOCTYPE html>
-	<html>
-	<head>
-		<title></title>
-	</head>
-	<body>
-	
-	</body>
-	</html>
-$>
+<?php
+	$url = $_SERVER['REDIRECT_URL'];
+	$baseURL= '/FunCoding';
+	if($_SERVER["REQUEST_METHOD"]=="GET"){
+		switch ($url){
+			case $baseURL."/homepage":
+				require_once "Controller/homeController.php";
+				$home = new HomeController();
+				echo $home ->start();
+				break;
+		}
+	}
+?>
